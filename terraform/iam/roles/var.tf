ec00617dcbdb0d76381b8d8da5aee2489a67a1d8
camilaleniss <mlmariacami2@gmail.com>
2024-02-27 07:00:16 -0500

    Add init infra for iam roles and policies

---
+++ terraform/iam/roles/var.tf
@@ -0,0 +1,7 @@
+variable "rds_policy_arn" {
+  type = string
+}
+
+variable "log_policy_arn" {
+  type = string
+}
---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket = "camileniss-terraform"
+    key    = "terraform.tfstate"
+    region = "us-east-2"
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "iam" {
+  source = "./iam"
+}
